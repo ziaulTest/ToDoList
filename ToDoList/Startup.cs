c958e9980fed9da3997c7bc43f104e74113cac4d
Ziaul Islam <ziaul.islam@asos.com>
2018-08-16 16:20:26 +0100

    made changes to the code, so now it will call from the repo rather then doing it straight from the controller.

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ToDoList.Interface;
 
 namespace ToDoList
 {
@@ -24,6 +25,7 @@ namespace ToDoList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<IToDoRepository, ToDoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
