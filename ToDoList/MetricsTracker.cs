aed29462b7891501ea692957a0e994f3db3ddef2
Ziaul Islam <ziaul.islam@asos.com>
2018-08-23 08:36:37 +0100

    AI

---
+++ ToDoList/MetricsTracker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+
+namespace ToDoList
+{
+    public class MetricsTracker
+    {
+        private readonly TelemetryClient _telemetryClient;
+
+        public MetricsTracker( TelemetryClient telemetryClient)
+        {
+            this._telemetryClient = telemetryClient ?? throw new ArgumentNullException(nameof(telemetryClient));
+        }
+
+        public void Customlog()
+        {
+            //_telemetryClient.TrackException();
+        }
+    }
+}
--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -19,6 +19,7 @@ namespace ToDoList
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
