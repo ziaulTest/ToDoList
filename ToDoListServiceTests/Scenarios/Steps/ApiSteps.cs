4fbed8370b3a12c0df0ebc7c226be2d699e90fda
Ziaul Islam <ziaul.islam@asos.com>
2018-08-29 09:31:45 +0100

    added a service test proj

---
+++ ToDoListServiceTests/Scenarios/Steps/ApiSteps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ToDoListServiceTests.Scenarios.Steps
+{
+    public class ApiSteps
+    {
+        private HttpClient httpClient;
+
+        public void ToDoList_Is_Available()
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:49469")
+            };
+        }
+
+        public void The_List_Is_Called()
+        {
+            var response = new Uri("api/ToDoLists", UriKind.Relative);
+            Task<HttpResponseMessage> sut = httpClient.GetAsync(response);
+        }
+
+        public void Response_Is_returned_With_Ok()
+        {
+            //var content = sut.Result.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
---
+++ ToDoServiceTests/Scenarios/EndToEnd/ToDoGetLists.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using TestStack.BDDfy;
+
+namespace ToDoServiceTests.Scenarios.EndToEnd
+{
+    [TestFixture]
+    [Story(AsA = "ToDoList Owner",
+        IWant = "To get the list of todo items",
+        SoThat = "I  can view my todo list",
+        Title = "x-x-x-x-x - GetToDoList - retrieve information from the todo list")]
+    public class ToDoGetLists : ApiSteps
+    {
+        [Test]
+        public void Given_A_Todo_List__When_Get_Is_Called__Then_The_Response_Is__OK()
+        {
+            this.Given(_ => _.ToDoList_Is_Available())
+                .When(_ => _.The_List_Is_Called())
+                .Then(_ => _.Response_Is_returned_With_Ok())
+                .BDDfy();
+        }
+
+    }
+}
