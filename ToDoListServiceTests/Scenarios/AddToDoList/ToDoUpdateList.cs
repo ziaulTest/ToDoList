5024c9ed9ac3ef7d550c5a767a10dc24c173f127
Ziaul Islam <ziaul.islam@asos.com>
2018-08-30 09:28:50 +0100

    service test for updating todolist

--- ToDoListServiceTests/Scenarios/AddToDoList/ToDoUpdateList.cs
+++ ToDoListServiceTests/Scenarios/AddToDoList/ToDoUpdateList.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using NUnit.Framework;
@@ -10,11 +11,11 @@ using ToDoList.Models;
 namespace ToDoListServiceTests.Scenarios.AddToDoList
 {
     [TestFixture]
-    public class ToDoAddList
+    public class ToDoUpdateList
     {
-        private HttpClient _httpClient;
-        private Uri _requestUri;
-        private HttpResponseMessage _sut;
+        private HttpClient httpClient;
+        private Uri requestUri;
+        private HttpResponseMessage sut;
 
         [Test]
         public void Given_A_TodoList_Is_Being_Viewed_When_A_User_Tries_To_Adds_A_ToDoList_Task_And_They_Can_Enter_A_Task_And_Priority_Then_The_Response_is_Ok()
@@ -22,23 +23,23 @@ namespace ToDoListServiceTests.Scenarios.AddToDoList
             this.Given(_ => _.ToDoList_Is_Available())
                 .When(_ => _.The_List_Is_Then_Called())
                 .And(_ => _.Add_Task_And_Priority_To_An_existing_List())
-                .Then(_ => _.Response_Is_returned_With_Ok())
+                .Then(_ => _.Response_Is_returned_With_Created())
                 .BDDfy();
         }
 
         public void ToDoList_Is_Available()
         {
-            this._httpClient = new HttpClient
+            httpClient = new HttpClient
             {
                 BaseAddress = new Uri("http://localhost:49469")
             };
 
-            _requestUri = new Uri("api/ToDoLists/1", UriKind.Relative);
+            requestUri = new Uri("api/ToDoLists/1", UriKind.Relative);
         }
 
         public async Task Add_Task_And_Priority_To_An_existing_List()
         {
-            ToDoListItems data = new ToDoListItems
+            var data = new ToDoListItems
             {
                 task = "added Via service Test",
                 priority = "High"
@@ -47,20 +48,20 @@ namespace ToDoListServiceTests.Scenarios.AddToDoList
             var convertToJson = JsonConvert.SerializeObject(data);
             var buffer = System.Text.Encoding.UTF8.GetBytes(convertToJson);
             var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
-            _sut = await _httpClient.PostAsync(_requestUri, byteContent);
-           
+            sut = await httpClient.PostAsync(requestUri, byteContent);
         }
 
         public async Task The_List_Is_Then_Called()
         {
-            _sut = await _httpClient.GetAsync(_requestUri);
+            sut = await httpClient.GetAsync(requestUri);
         }
 
-        public void Response_Is_returned_With_Ok()
+        public void Response_Is_returned_With_Created()
         {
-            var content = _sut.Content.ReadAsStringAsync().Result;
-            Assert.AreEqual(_sut.StatusCode, HttpStatusCode.OK, content);
+            // var content = sut.Content.LoadIntoBufferAsync().ToString();
+            Assert.AreEqual(sut.StatusCode, HttpStatusCode.Created);
         }
     }
 }
