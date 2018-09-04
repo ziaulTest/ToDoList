cb05dfa7f7ed6e83223a69468ebdeee06b8aa6a0
Ziaul Islam <ziaul.islam@asos.com>
2018-09-04 08:26:24 +0100

    changes made to Api Steps

--- ToDoListServiceTests/Scenarios/Data Validation/BackEndApiRejectionToDoList.cs
+++ ToDoListServiceTests/Scenarios/Data Validation/BackEndApiRejectionToDoList.cs
@@ -11,7 +11,7 @@ namespace ToDoListServiceTests.Scenarios.Data_Validation
         [Test]
         public void Given_A_TodoList_Item_That_Needs_To_Be_Updated_When_A_User_Tries_To_Add_An_invalid_ToDoList_Item_Then_The_Response_is_NotFound()
         {
-            this.Given(_ => _.A_Invalid_Request_To_View_A_ToDoList())
+            this.Given(_ => _.A_Request_To_View_ToDoLists())
                 .When(_ => _.Invalid_Update_A_ToDoList_Item())
                 .Then(_ => _.Response_Is_returned_With_NotFound())
                 .BDDfy();
--- ToDoListServiceTests/Scenarios/DeleteToDoList/DeleteToDoList.cs
+++ ToDoListServiceTests/Scenarios/DeleteToDoList/DeleteToDoList.cs
@@ -10,7 +10,7 @@ namespace ToDoListServiceTests.Scenarios.DeleteToDoList
         [Test]
         public void Given_A_TodoList_Is_Being_Viewed_When_A_User_Tries_To_Delete_A_ToDoList_Then_The_Response_Returned_Is_NoContent()
         {
-            this.Given(_ => _.A_Request_To_View_A_ToDoList())
+            this.Given(_ => _.A_Request_To_View_A_Single_ToDoList())
                 .And(_ => _.Add_Item_To_A_List())
                 .When(_ => _.The_List_Is_Called())
                 .And(_=> _.Delete_An_Existing_List())
