aab7cabc83422a2b6bef8f8f8285ec84bf11048f
Ziaul Islam <ziaul.islam@asos.com>
2018-09-04 10:14:22 +0100

    minor changes to controller

--- ToDoListServiceTests/Scenarios/AddToDoList/AddToDoList.cs
+++ ToDoListServiceTests/Scenarios/AddToDoList/AddToDoList.cs
@@ -10,10 +10,9 @@ namespace ToDoListServiceTests.Scenarios.AddToDoList
         [Test]
         public void Given_A_TodoList_Is_Being_Viewed_When_A_User_Tries_To_Adds_A_ToDoList_Task_And_They_Can_Enter_A_Task_And_Priority_Then_The_Response_is_Created()
         {
-            this.Given(_ => _.A_Request_To_View_A_Single_ToDoList())
-                .When(_ => _.The_List_Is_Called())
-                .And(_ => _.Add_Item_To_An_existing_List())
-                .Then(_ => _.Response_Is_Returned_With_Status_Ok())
+            this.Given(_ => _.A_Request_To_Add_A_Single_ToDoList())
+                .When(_ => _.Add_Item_To_A_List())
+                .Then(_ => _.Response_Is_returned_With_Created())
                 .BDDfy();
         }
     }
